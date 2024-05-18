49b24d9ae0677dbe63b6803c831d6172173a9a90
WAVE MEDIA NETWORK <127257087+WaveMediaNetwork@users.noreply.github.com>
2024-05-18 19:42:33 +0300

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import RightSidebar from '@/components/ui/RightSidebar';
 
 const Home = () => {
+    const loggedIn = {firstName: 'John', lastName: 'JSM', email: 'micicoi.ionut@gmail.com'};
+
     return(
-        <div>Home</div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn.firstName || 'Guest'}
+                        subtext="Access and Manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox 
+                        accounts = {[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1275.50}
+                    />
+                </header>
+
+                RESENT TRANSACTIONS
+            </div>
+            <RightSidebar 
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+            />
+        </section>
     )
 
 }
